75f122eec8ac454b76b1a8952760482b30aa3b0a
Morgan N <mzartdesigns@gmail.com>
2017-09-20 15:07:11 -0400

    Trying to get the boxes function to work
    Unable to have the class “active” be removed from the first box when
    clicking the others….

--- script.js
+++ script.js
@@ -1,13 +1,106 @@
 var cSound = document.querySelector("#cAudio");
 var cNote = document.querySelector("#c");
 
-cNote.addEventListener("mousedown", function(){
+var dSound = document.querySelector("#dAudio");
+var dNote = document.querySelector("#d");
+
+var eSound = document.querySelector("#eAudio");
+var eNote = document.querySelector("#e");
+
+var fSound = document.querySelector("#fAudio");
+var fNote = document.querySelector("#f");
+
+var gSound = document.querySelector("#gAudio");
+var gNote = document.querySelector("#g");
+
+var aSound = document.querySelector("#aAudio");
+var aNote = document.querySelector("#a");
+
+var bSound = document.querySelector("#bAudio");
+var bNote = document.querySelector("#b");
+
+var musicBox = document.querySelector(".box");
+
+boxes();
+function boxes(){
+  // for(var i = 0; i < musicBox.length; i++){
+  //   musicBox[i].addEventListener("click", function(){
+  //     if(musicBox.classList.contains('active')){
+  //       musicBox.classList.remove("active");
+  //     } else{
+  //       musicBox.classList.add("active");
+  //     }
+  //   });
+  // }
+  musicBox.addEventListener("click", function(){
+    if(musicBox.className !== "active") {
+      musicBox.classList.add("active");
+    } else {
+      musicBox.classList.remove("active");
+    }
+    // musicBox.classList.remove("active");
+    // this.classList.add("active");
+  });
+}
+
+
+cNote.addEventListener("click", function(){
+
   cSound.currentTime = 0;
   cSound.play();
 
-  if (this.className === "active"){
-    this.classList.remove("active");
-  } else {
-    this.classList.add("active");
-  }
+
+  // if (cNote.className === "active"){
+  //   cNote.classList.remove("active");
+  // } else {
+  //   this.classList.add("active");
+  // }
+
+});
+
+dNote.addEventListener("click", function(){
+  dSound.currentTime = 0;
+  dSound.play();
+  boxes();
+
+});
+
+eNote.addEventListener("click", function(){
+  eSound.currentTime = 0;
+  eSound.play();
+
+
+
+});
+
+fNote.addEventListener("click", function(){
+  fSound.currentTime = 0;
+  fSound.play();
+
+
+
+});
+
+gNote.addEventListener("click", function(){
+  gSound.currentTime = 0;
+  gSound.play();
+
+
+
+});
+
+aNote.addEventListener("click", function(){
+  aSound.currentTime = 0;
+  aSound.play();
+
+
+
+});
+
+bNote.addEventListener("click", function(){
+  bSound.currentTime = 0;
+  bSound.play();
+
+
+
 });
